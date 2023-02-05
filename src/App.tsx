b5dd3a824139d89278bd3db45017f4cadf88286b
Nikos00007 <alfreditonik@gmail.com>
2023-02-05 13:03:38 +0100

    hooks useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import React,{FunctionComponent, useState} from 'react';
+import React,{FunctionComponent, useState,useEffect} from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mocks-pokemon';
   
 const App: FunctionComponent = () => {
- const [name]=useState('react');   
+ const [name]=useState('react')   
  
- const [pokemons]=useState<Pokemon[]>(POKEMONS);
+ const [pokemons,setPokemons]=useState<Pokemon[]>([]);
+ useEffect(()=>{
+     setPokemons(POKEMONS);
+ },[]);
     
  return (
      <div>
          <h1>Pokedex</h1>
          <p>
-             Il y a {pokemons.length} pokemons  dans la Pokedex
-             {name}
+             Il y a {pokemons.length} pokemons dans la Pokedex
+
          </p>
 
 
