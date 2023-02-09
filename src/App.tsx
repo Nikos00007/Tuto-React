80e1f4fc23c2049562c6c08cfdf9c8eac5b76ce5
Nikos00007 <alfreditonik@gmail.com>
2023-02-09 05:49:01 +0100

    map utilisation

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,50 @@
-import React,{FunctionComponent, useState,useEffect} from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mocks-pokemon';
-  
+import { idText } from 'typescript';
+
 const App: FunctionComponent = () => {
- const [name]=useState('react')   
- 
- const [pokemons,setPokemons]=useState<Pokemon[]>([]);
- useEffect(()=>{
-     setPokemons(POKEMONS);
- },[]);
-    
- return (
-     <div>
-         <h1>Pokedex</h1>
-         <p>
-             Il y a {pokemons.length} pokemons dans la Pokedex
-
-         </p>
 
 
-     </div>
+    const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+    useEffect(() => {
+        setPokemons(POKEMONS);
+    }, []);
 
- )
+    return (
+        <div >
+            <h1 className="center">Pokedex</h1>
+            <div className="container" >
+                <div className="row">
+                    {pokemons.map(({id, name, picture, created}) =>(
+                    <div className='col-6 col-sm-4' key={id}>
+                        <div className='card horizontal'>
+                            <div className='card-img'>
+                                <img src={picture} alt={name} />
+                            </div>
+                            <div className='card-stacked'>
+                                <div className='card-body'>
+                                    <p>{name} </p>
+                                    <p> <small>{created.toString()} </small> </p>:
+                                </div>
+
+                            </div>
+
+                        </div>
+
+
+
+                    </div>
+                    ))}
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+    )
 }
-  
+
 export default App;
